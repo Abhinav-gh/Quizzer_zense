cda03399bf4c752d1a594b2ea9ccda8c33bc068d
Abhinav <Abhinav.Deshpande@iiit.ac.in>
2023-08-08 22:11:52 +0530

    Added previous and storing answer

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -35,30 +35,47 @@ let questions = [
         ],
     },
 ];
-let marked_answer=[];
+let already_answered=[0,0,0];
+let marked_answer=["","",""];
 
-let question_count=0;
+let question_count=-1;
 let points=0;
 
-document.addEventListener("DOMContentLoaded", show(question_count));
-    
+document.addEventListener("DOMContentLoaded", () => {
+    show(question_count, check_already_answered());
+  });    
 
 
-function show(count) {
-    let question = document.getElementById("questions");
-    let [first, second, third, fourth] = questions[count].options; 
-    question.innerHTML = `<div class="question"><h2>Q${count + 1}.${questions[count].question}</h2></div>
+function show(count,already_marked=0) {
+    console.log("Marked answer    " , marked_answer);
+    if (count >= 0 && count < questions.length) {
 
-      <ul class="option_group">
-      <li class="option">${first} </li>
-      <li class="option">${second} </li>
-      <li class="option">${third} </li>
-      <li class="option">${fourth} </li>
-      </ul>`;
-    toggleActive();
-  }
-function toggleActive(){
-    let option= document.querySelectorAll( "li.option");
+        let question = document.getElementById("questions");
+        let [first, second, third, fourth] = questions[count].options; 
+        question.innerHTML = `<div class="question"><h2>Q${count + 1}.${questions[count].question}</h2></div>
+
+        <ul class="option_group">
+        <li class="option">${first} </li>
+        <li class="option">${second} </li>
+        <li class="option">${third} </li>
+        <li class="option">${fourth} </li>
+        </ul>`;
+        question_count = count;
+        toggleActive(already_marked);
+    }
+}
+function toggleActive(already_marked=0){
+    console.log("already marked ", already_marked);
+    let option= document.querySelectorAll("li.option");
+    console.log("option: ", option );
+    if (already_marked == 1) {
+        for (let i = 0; i < option.length; i++) {
+          option[i].classList.remove("active"); 
+          if (option[i].textContent.trim() === marked_answer[question_count]) {
+            option[i].classList.add("active"); 
+            }
+        }
+    }
     for(let i=0;i<option.length;i++)
     {
         option[i].onclick = function(){
@@ -67,25 +84,55 @@ function toggleActive(){
                     option[i].classList.remove("active");
                 }
             }
-            option[i].classList.add("active");        
+            option[i].classList.add("active");
         };
     }
 }
-function next(){
-    let user_answer=document.querySelector("li.option.active");
-    user_answer = user_answer.textContent.trim();
-    marked_answer.push(user_answer);
+function store_answer() {
+    let user_answer = document.querySelector("li.option.active");
+    console.log("user_answer:", user_answer);
+
+    user_answer = user_answer ? user_answer.textContent.trim() : marked_answer[question_count];
+    console.log("user_answer trimmed:", user_answer);
+
     
-    if(user_answer == questions[question_count].answer){
+        if (already_answered[question_count] === 0)
+            already_answered[question_count] = 1;
         console.log("Here");
-        points=points+10;
-        sessionStorage.setItem("points",points);
-    }
+        marked_answer[question_count] = user_answer;
+        console.log("marked_answer after update:", marked_answer);
+    
+}
+
+
+function check_already_answered(){
+    if(already_answered[question_count]==1)
+        return 1;
+    return 0;
+}
+function next(){
     if(question_count==questions.length-1){
+        calculate_score();
         location.href="final.html";
     }
     question_count++;
-    console.log(points);
-    console.log(question_count);
-    show(question_count);
+    store_answer();
+    show(question_count,check_already_answered());
+    
+}
+function previous(){
+    if(question_count==0)
+        return;
+    question_count--;
+    store_answer();
+    show(question_count,check_already_answered());
+    
+}
+function calculate_score(){
+    points=0;
+    for(let i=0;i<questions.length;i++)
+        if(marked_answer[i]== questions[i].answer)
+            points+=10;
+    sessionStorage.setItem("points",points);
+    
 }
