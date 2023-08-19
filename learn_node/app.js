63f49412f48b5722ca40a91988a3264b934d2623
Abhinav <Abhinav.Deshpande@iiit.ac.in>
2023-08-19 21:03:02 +0530

    Finally set up my original project as an express app. Still finishing database connection is underway and not yet complete

--- learn_node/app.js
+++ learn_node/app.js
@@ -2,13 +2,27 @@ var express = require('express');
 var fortune = require('./library/fortune.js');
 
 var app = express();
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+app.disable('x-powered-by');
 app.get('/', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    res.render('about', { fortune: fortune.getFortune()});
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
@@ -24,7 +38,9 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
-app.use(express.static( __dirname +'/public'));
+
+app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' +
@@ -39,4 +55,3 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-   
